b9c7d1121c171154ea577ec7c86c1157a3646e47
Connor Shaw <glacier526@gmail.com>
2024-11-13 16:50:05 -0500

    set lp test

--- script/c210000046.lua
+++ script/c210000046.lua
@@ -10,15 +10,6 @@ function s.initial_effect(c)
 	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e0:SetOperation(s.e0evt)
 	c:RegisterEffect(e0)
-	--[[
-	local e0b=Effect.CreateEffect(c)
-	e0b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e0b:SetCode(EVENT_CHAINING)
-	e0b:SetRange(LOCATION_FZONE)
-	e0b:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e0b:SetOperation(s.e0evt)
-	c:RegisterEffect(e0b)
-	]]--
 	-- [Activation]
 	-- Activate this card by sending all monsters you control to the GY (min. 0).
 	local e1=Effect.CreateEffect(c)
@@ -100,8 +91,7 @@ end
 function s.e0evt(e,tp)
 	local g=Duel.GetMatchingGroup(s.e0fil,tp,LOCATION_ONFIELD,0,nil)
 	if g:GetClassCount(Card.GetCode)>=5 then
-		Debug.ShowHint("WIN") -- does Proc correctly
-		--Duel.Win(tp,WIN_REASON_DESTINY_BOARD)
+		Duel.SetLP(1-tp, 0)
 	end
 end
 function s.e1cst(e,tp,eg,ep,ev,re,r,rp,chk)
