1a1fd78558b2cd52103650bb9767461c14c3a3f2
Connor Shaw <glacier526@gmail.com>
2024-11-14 00:38:25 -0500

    add

--- script/c210000058.lua
+++ script/c210000058.lua
@@ -1,11 +1,11 @@
--- Rainbow Realm Contortion
+-- Rainbow Realm Reconstruction
 local s,id,o=GetID()
--- c210000054
+-- c210000058
 function s.initial_effect(c)
 	--[[
 	[HOPT]
-	If you control no monsters:
-	Place 1 “Area” Continuous Trap with a different name from the cards you control from your hand, Deck, or GY face-up on the field.
+	If you control no monsters: Place 1 “Rainbow Realm of Doom” from your Deck or GY face-up in your Field Zone.
+	While that card is in the Field Zone, it cannot be banished or shuffled into the Deck by card effects.
 	You cannot Summon/Set monsters the turn you activate this effect.
 	]]--
 	local e1=Effect.CreateEffect(c)
@@ -52,13 +52,16 @@ function s.initial_effect(c)
 	end)
 	--[[
 	[HOPT]
-	During the Main Phase: You can banish this card from your GY;
-	you take no effect damage for the rest of this turn.
+	During your opponent’s Battle Phase: You can banish this card from your GY;
+	you take no battle damage for the rest of this turn.
 	]]--
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,(id+1))
+	e2:SetHintTiming(0,TIMING_BATTLE_START)
+	e2:SetCondition(s.e2con)
 	e2:SetCost(aux.bfgcost)
 	e2:SetOperation(s.e2evt)
 	c:RegisterEffect(e2)
@@ -68,18 +71,12 @@ function s.e1con(e,tp)
 	and not Duel.HasFlagEffect(tp,id)
 end
 function s.e1fil(c,tp)
-	return c:IsSetCard(0xce2b)
-	and c:IsContinuousTrap()
+	return c:IsCode(210000046)
 	and not c:IsForbidden()
-	and not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,c:GetCode()),tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.e1tgt(e,tp,eg,ep,ev,re,r,rp,chk)
-	local z=Duel.GetLocationCount(tp,LOCATION_SZONE)
-	if e:GetHandler():IsLocation(LOCATION_HAND) then z=z-1 end
-
 	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.e1fil,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,tp)
-		and z>0
+		return Duel.IsExistingMatchingCard(s.e1fil,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,tp)
 	end
 end
 function s.e1evt(e,tp)
@@ -117,33 +114,52 @@ function s.e1evt(e,tp)
 	e1a4:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1a4,tp)
 
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<1 then return end
-
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectMatchingCard(tp,s.e1fil,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.e1fil,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp)
 	if g:GetCount()>0 then
-		Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		local tc=g:GetFirst()
+		if Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true) then
+			local e1b1=Effect.CreateEffect(c)
+			e1b1:SetType(EFFECT_TYPE_FIELD)
+			e1b1:SetCode(EFFECT_CANNOT_REMOVE)
+			e1b1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+			e1b1:SetRange(LOCATION_FZONE)
+			e1b1:SetTargetRange(LOCATION_FZONE,LOCATION_FZONE)
+			e1b1:SetValue(1)
+			e1b1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1b1,true)
+
+			local e1b2=Effect.CreateEffect(c)
+			e1b2:SetType(EFFECT_TYPE_FIELD)
+			e1b2:SetCode(EFFECT_CANNOT_TO_DECK)
+			e1b2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+			e1b2:SetRange(LOCATION_FZONE)
+			e1b2:SetTargetRange(LOCATION_FZONE,LOCATION_FZONE)
+			e1b2:SetValue(1)
+			e1b2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1b2,true)
+		end
 	end
 end
-function s.e2evt(e,tp,eg,ep,ev,re,r,rp)
-	local e2a=Effect.CreateEffect(e:GetHandler())
+function s.e2con(e,tp)
+	return (Duel.GetCurrentPhase()==PHASE_BATTLE_START or Duel.IsBattlePhase())
+	and Duel.IsTurnPlayer(1-tp)
+end
+function s.e2evt(e,tp)
+	local c=e:GetHandler()
+
+	local e2a=Effect.CreateEffect(c)
 	e2a:SetType(EFFECT_TYPE_FIELD)
-	e2a:SetCode(EFFECT_CHANGE_DAMAGE)
+	e2a:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
 	e2a:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2a:SetTargetRange(1,0)
-	e2a:SetValue(s.e2val)
-	e2a:SetReset(RESET_PHASE+PHASE_END)
+	e2a:SetValue(1)
+	e2a:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e2a,tp)
 
-	local e2b=e2a:Clone()
-	e2b:SetCode(EFFECT_NO_EFFECT_DAMAGE)
-	e2b:SetReset(RESET_PHASE+PHASE_END)
+	local e2b=Effect.CreateEffect(c)
+	e2b:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_OATH)
+	e2b:SetReset(RESET_PHASE|PHASE_END)
+	e2b:SetTargetRange(1,0)
 	Duel.RegisterEffect(e2b,tp)
-end
-function s.e2val(e,re,val,r)
-	if (r&REASON_EFFECT)~=0 then
-		return 0
-	else
-		return val
-	end
 end
