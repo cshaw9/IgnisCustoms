c539eed120f11081f8d8aa73b7a3c420fd3dcd83
Connor Shaw <glacier526@gmail.com>
2024-11-13 20:21:43 -0500

    test count

--- script/c210000053.lua
+++ script/c210000053.lua
@@ -24,31 +24,33 @@ function s.e1fil(c)
 end
 function s.e1tgt(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.e1fil,tp,LOCATION_HAND,0,nil)
-	local max=Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)
-	if max>g:GetCount() then
-		max=g:GetCount()
-	end
 
 	if chk==0 then
 		return g:GetCount()>0
-		and Duel.IsPlayerCanDraw(tp,max+1)
+		and Duel.IsPlayerCanDraw(tp,2)
+	end
+
+	local max=Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)
+	if max>(g:GetCount()+1) then
+		max=g:GetCount()
 	end
 
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local sel=g:Select(tp,1,max,nil)
-	Duel.ConfirmCards(1-tp,sel)
-
+	
+	local sel=g:Select(tp,1,max-1,nil)
 	e:SetLabelObject(sel)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,max+1)
+
+	Duel.ConfirmCards(1-tp,sel)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,max)
 end
 function s.e1evt(e,tp)
 	local g=Duel.GetMatchingGroup(s.e1fil,tp,LOCATION_HAND,0,nil)
 	local max=Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)
-	if max>g:GetCount() then
+	if max>(g:GetCount()+1) then
 		max=g:GetCount()
 	end
 
-	if Duel.Draw(tp,max+1,REASON_EFFECT)>0 then
+	if Duel.Draw(tp,max,REASON_EFFECT)>0 then
 		local tg=e:GetLabelObject()
 
 		local rt=Duel.SendtoDeck(tg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
