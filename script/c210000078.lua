b7cd9299aa1570d0437086a58fc7a744311a126f
Connor Shaw <glacier526@gmail.com>
2024-11-14 19:07:19 -0500

    debug

--- script/c210000078.lua
+++ script/c210000078.lua
@@ -13,7 +13,6 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,(id+0))
 	e1:SetCost(s.e1cst)
-	e1:SetTarget(s.e1tgt)
 	e1:SetOperation(s.e1evt)
 	c:RegisterEffect(e1)
 end
